6f02b01625c2a41d1ab64892f0b5af002ef90daa
dieegogutierrez <dieego_gutierrez@hotmail.com>
2024-08-01 06:46:49 -0400

    add hidden field to contact form

--- website/routes.py
+++ website/routes.py
@@ -3,7 +3,7 @@ import requests
 from website import app, mail, recaptcha
 from website.forms import MyForm
 from datetime import datetime
-from flask import render_template, request
+from flask import render_template, request, redirect, flash, url_for
 from flask_mail import Message
 
 
@@ -36,12 +36,25 @@ def contact():
     recaptcha_secret_key = app.config['RECAPTCHA_SECRET_KEY']
     
     if form.validate_on_submit():
+        # Get the honeypot field value
+        honeypot = request.form.get('username')
+        
+        # Check if the honeypot field is filled
+        if honeypot:
+            # Honeypot is filled; treat this as a bot submission
+            flash('Your submission was flagged as spam.', 'danger')
+            return redirect(url_for('contact'))
+        
+        # Get the reCAPTCHA response token from the form
         recaptcha_response = request.form.get('recaptcha-token')
+        
+        # Verify the reCAPTCHA response with Google's API
         payload = {'secret': recaptcha_secret_key, 'response': recaptcha_response}
         response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=payload)
         result = response.json()
 
         if result.get('success'):
+            # If reCAPTCHA validation is successful, send the email
             msg = Message('New Message',
                           sender=os.getenv('MAIL_USERNAME'),
                           recipients=[os.getenv('MAIL_HOTMAIL')])
@@ -52,6 +65,7 @@ def contact():
             mail.send(msg)
             return render_template("contact.html", msg_sent=True, form=form, recaptcha_site_key=recaptcha_site_key)
         else:
+            # If reCAPTCHA validation fails, display an error
             return render_template("contact.html", msg_sent=False, form=form, recaptcha_site_key=recaptcha_site_key, recaptcha_failed=True)
     
     return render_template("contact.html", msg_sent=False, form=form, recaptcha_site_key=recaptcha_site_key)
